0c46e3fffbce1a6f0365168b11cd4feec22b0a90
StephenAgyemang <StephenAsamoahAgyemang@outlook.com>
2024-05-18 13:17:43 +0000

    Task 2 commit

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -16,3 +16,22 @@ function processArray (nums){
 nums = [3,5,6,9,0]
 console.log(processArray(nums))
 
+//Task 2
+
+function formatArrayStrings(strings, numbers) {
+  return strings.map((str, index) => {
+    const num = numbers[index];
+    return num % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+  });
+}
+
+// Task 2 usage example
+function processArray(arr) {
+  return arr.map(num => num % 2 === 0 ? num * num : num * 3);
+}
+
+const strings = ['Nana', 'Kwame', 'Asamoah', 'Agyemang', 'Junior'];
+const numbers = processArray([1, 2, 3, 4, 5]);
+const formattedStrings = formatArrayStrings(strings, numbers);
+
+console.log(formattedStrings);
